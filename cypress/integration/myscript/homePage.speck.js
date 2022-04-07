ebf1eaee5c954472a26c34b56147e9b9ef93a7d6
YirguBira <yirgunig@gmail.com>
2022-04-07 13:12:58 -0400

    7-2-22-1 created command.add()

--- cypress/integration/myscript/homePage.speck.js
+++ cypress/integration/myscript/homePage.speck.js
@@ -1,5 +1,7 @@
 describe('user is on home page', () =>{
     it('asserts user is on home page', () =>{
+        cy.eventNames()
+        cy.name()
         cy.visit('/')
         cy.get(':nth-child(1) > .gb_d').should("have.text", "Gmail")
         
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,7 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+Cypress.Commands.add('name', (overrides = {}) => { 
+   const username = Cypress.env('username')
+    cy.log(username)
+})
