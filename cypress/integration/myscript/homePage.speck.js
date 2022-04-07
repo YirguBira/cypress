3cda50d26f942249698c107886164570b2803796
YirguBira <yirgunig@gmail.com>
2022-04-07 19:39:18 -0400

    7-2-22-1 corrected Cypress.env() for var set via cli

--- cypress/integration/myscript/homePage.speck.js
+++ cypress/integration/myscript/homePage.speck.js
@@ -1,7 +1,6 @@
 describe('user is on home page', () =>{
     it('asserts user is on home page', () =>{
-        const y = cy.name()
-        y.should('equal',"YirguBira")
+        cy.name().should('equal',"YirguBira")
         cy.visit('/')
         cy.get(':nth-child(1) > .gb_d').should("have.text", "Gmail")
         cy.log({
